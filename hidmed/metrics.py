0da4b8fc7611fc0017269bac051af9e55571e7f8
Alan Yang <ayang1097@gmail.com>
2024-04-28 22:37:36 -0700

    finish experiment setup

--- hidmed/metrics.py
+++ hidmed/metrics.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def is_covered(psi, psi_true):
+def is_covered(psi, true_psi):
     """check if true value is covered by estimates with 95% confidence"""
     mean = np.mean(psi)
     ci = 1.96 * np.std(psi) / np.sqrt(len(psi))
-    return psi_true <= mean + ci and psi_true >= mean - ci
+    return true_psi <= mean + ci and true_psi >= mean - ci
 
 
 def confidence_interval(x):
@@ -13,8 +13,13 @@ def confidence_interval(x):
     return 2 * (1.96 * np.std(x) / np.sqrt(len(x)))
 
 
-def calculate_mse(psi, psi_true):
+def calculate_mse(psi, true_psi):
     """calculate MSE of estimate"""
-    bias = np.mean(psi) - psi_true
+    bias = np.mean(psi) - true_psi
     var = np.var(psi) / len(psi)
     return var + bias**2
+
+
+def absolute_normalized_bias(psi, true_psi):
+    """calculate absolute normalized bias of estimate"""
+    return np.abs(np.mean(psi) - true_psi) / np.abs(true_psi)
